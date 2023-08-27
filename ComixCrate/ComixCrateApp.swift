90a296295c7f4827f7b8ad273d9354e307413484
Ben Carney <ben@thistlenthorn.com>
2023-08-27 12:18:35 -0700

    Initial Commit

---
+++ ComixCrate/ComixCrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComixCrateApp.swift
+//  ComixCrate
+//
+//  Created by Ben Carney on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ComixCrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComixCrate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComixCrate
+//
+//  Created by Ben Carney on 8/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
